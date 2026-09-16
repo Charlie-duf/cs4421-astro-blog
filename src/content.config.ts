96c82d649c6297cab91d3025e19d1b493db25c11
Charlie-duf <charliedu2008@gmail.com>
2026-09-16 10:21:53 +0100

    feat: initial scaffold of Astro blog platform

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
 		z.object({
 			title: z.string(),
 			description: z.string(),
+				author: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
@@ -17,4 +18,22 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+	const authors = defineCollection({
+		loader: glob({ base: './src/content/authors', pattern: '**/*.{md,mdx}' }),
+		schema: ({ image }) =>
+			z.object({
+				name: z.string(),
+				bio: z.string(),
+				avatar: image(),
+				socialLinks: z
+					.array(
+						z.object({
+							label: z.string(),
+							url: z.string().url(),
+						}),
+					)
+					.default([]),
+			}),
+	});
+
+	export const collections = { blog, authors };
